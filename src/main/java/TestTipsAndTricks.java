fde9291c58be8bd46a4cf2d81059387f1ea70846
eros <eroskoller@gmail.com>
2014-06-03 14:55:31 -0300

    tipsAndTricks

--- src/main/java/TestTipsAndTricks.java
+++ src/main/java/TestTipsAndTricks.java
@@ -1,5 +1,7 @@
 
 import br.com.zeros.tipsandtricks.StringsUtils;
+import br.com.zeros.tipsandtricks.cryto.EncryptionStrings;
+import br.com.zeros.tipsandtricks.cryto.HMACDigest;
 import br.com.zeros.tipsandtricks.io.IOUtils;
 import java.util.List;
 
@@ -15,7 +17,27 @@ import java.util.List;
  */
 public class TestTipsAndTricks {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        
+//        String txt = "\n Brasília - O ministro Joaquim Barbosa deve confirmar a sua aposentadoria no início da sessão plenária desta quinta-feira (29/5), "
+//                + "\n em comunicação aos seus colegas. Ele almoçou com seus assessores no restaurante do Supremo Tribunal Federal, "
+//                + "\n mas a segurança não permitiu a aproximação dos repórteres. Não se sabe ainda se ele se aposenta em junho, "
+//                + "\n ou se depois da Copa do Mundo, em julho. Especula-se que o pedido de aposentadoria seria formalizado no dia 25 de junho, "
+//                + "\n exatamente 11 anos depois de sua posse como ministro do Supremo, em 2003, nomeado pelo então presidente Luiz Inácio da Silva. "
+//                + "\n Seu sucessor, pelo sistema de rodízio, é o atual vice-presidente, Ricardo Lewandowski. Em seu discurso, "
+//                + "\n Barbosa deverá usar como justificativa problemas de saúde e afirmar que não tem projetos políticos.";
+//         txt += txt; //txt += txt; txt += txt; txt += txt; txt += txt;
+//        String valueEnc = EncryptionStrings.encrypt(txt);
+//        String valueDec = EncryptionStrings.decrypt(valueEnc);
+//
+//        System.out.println("Plain Text : " + txt);
+//        System.out.println("Encrypted : \n" + valueEnc);
+//        System.out.println("Decrypted : " + valueDec);
+//        System.out.println("DigestHmac Original: \n"+HMACDigest.hmacDigestHmac(txt, EncryptionStrings.myEncryptionKey));
+//        System.out.println("DigestHmac  Refactory: \n"+HMACDigest.hmacDigestHmac(valueDec, EncryptionStrings.myEncryptionKey));
+        
+        
+        
 //        System.out.println(OsUtils.isUnix());
         
 //        System.out.println(Cpf.formatingCpf("21304365875"));
@@ -34,7 +56,7 @@ public class TestTipsAndTricks {
 //        IOUtils.deleteFilesByExtension("/home/eros/Documents/", "*.eros");
         
         
-        System.out.println(StringsUtils.replaceNoASCII("éros glauço kóller"));
+//        System.out.println(StringsUtils.replaceNoASCII("éros glauço kóller"));
         
     }
     
--- src/main/java/br/com/zeros/tipsandtricks/cryto/EncryptionStrings.java
+++ src/main/java/br/com/zeros/tipsandtricks/cryto/EncryptionStrings.java
@@ -17,8 +17,8 @@ import org.apache.commons.codec.binary.Base64;
  */
 public class EncryptionStrings {
 
+    public static final String myEncryptionKey = "$*%&ß|@#|ç-_?!";
     private static final String ALGORITHM = "AES";
-    private static final String myEncryptionKey = "$*%&ß|@#|ç-_?!";
     private static final String UNICODE_FORMAT = "UTF8";
 
     private static Key generateKey() throws Exception {
@@ -48,25 +48,6 @@ public class EncryptionStrings {
         return decryptedValue;
     }
 
-    public static void main(String[] args) throws Exception {
-
-         String txt = "\n Brasília - O ministro Joaquim Barbosa deve confirmar a sua aposentadoria no início da sessão plenária desta quinta-feira (29/5), "
-                + "\n em comunicação aos seus colegas. Ele almoçou com seus assessores no restaurante do Supremo Tribunal Federal, "
-                + "\n mas a segurança não permitiu a aproximação dos repórteres. Não se sabe ainda se ele se aposenta em junho, "
-                + "\n ou se depois da Copa do Mundo, em julho. Especula-se que o pedido de aposentadoria seria formalizado no dia 25 de junho, "
-                + "\n exatamente 11 anos depois de sua posse como ministro do Supremo, em 2003, nomeado pelo então presidente Luiz Inácio da Silva. "
-                + "\n Seu sucessor, pelo sistema de rodízio, é o atual vice-presidente, Ricardo Lewandowski. Em seu discurso, "
-                + "\n Barbosa deverá usar como justificativa problemas de saúde e afirmar que não tem projetos políticos.";
-         txt += txt; //txt += txt; txt += txt; txt += txt; txt += txt;
-        String valueEnc = EncryptionStrings.encrypt(txt);
-        String valueDec = EncryptionStrings.decrypt(valueEnc);
-
-        System.out.println("Plain Text : " + txt);
-        System.out.println("Encrypted : \n" + valueEnc);
-        System.out.println("Decrypted : " + valueDec);
-        System.out.println("DigestHmac Original: \n"+HMACDigest.hmacDigestHmac(txt, myEncryptionKey));
-        System.out.println("DigestHmac  Refactory: \n"+HMACDigest.hmacDigestHmac(valueDec, myEncryptionKey));
-        
-    }
+    
 
 }
